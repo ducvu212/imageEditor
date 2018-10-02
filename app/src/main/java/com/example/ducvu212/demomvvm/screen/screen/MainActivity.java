ee191d6a9da75485aeb0b2ac9d07f78da39787d6
huyhung1907 <hunghuy1907@gmail.com>
2018-10-03 01:39:07 +0700

    xml home screen

--- app/src/main/java/com/example/ducvu212/demomvvm/screen/screen/MainActivity.java
+++ app/src/main/java/com/example/ducvu212/demomvvm/screen/screen/MainActivity.java
@@ -1,14 +1,93 @@
 package com.example.ducvu212.demomvvm.screen.screen;
 
-import android.support.v7.app.AppCompatActivity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import com.example.ducvu212.demomvvm.R;
+import com.example.ducvu212.demomvvm.databinding.ActivityHomeBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    private ActivityHomeBinding mHomeBinding;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mHomeBinding = DataBindingUtil.setContentView(this, R.layout.activity_home);
+
+        Toolbar toolbar = mHomeBinding.appbar.toolbar;
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = mHomeBinding.drawerLayout;
+        ActionBarDrawerToggle toggle =
+                new ActionBarDrawerToggle(this, drawer, toolbar,
+                        R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = mHomeBinding.navView;
+        navigationView.setNavigationItemSelectedListener(this);
+
+        displaySelectedScreen(R.id.nav_home);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_search) {
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        displaySelectedScreen(menuItem.getItemId());
+        return true;
+    }
+
+    public void displaySelectedScreen(int itemId) {
+        Fragment fragment = null;
+        switch (itemId) {
+            case R.id.nav_home:
+                break;
+            case R.id.nav_offline:
+                break;
+            case R.id.nav_search:
+                break;
+            case R.id.nav_about_us:
+                break;
+        }
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction().add(R.id.frame_main, fragment).commit();
+        }
+        DrawerLayout drawer = mHomeBinding.drawerLayout;
+        drawer.closeDrawer(GravityCompat.START);
     }
 }
